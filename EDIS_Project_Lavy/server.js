9432e65b537a6b7b4dac53676c4d7ce9d661e2e1
LavanyaR <lrangana@andrew.cmu.edu>
2017-08-03 11:47:46 -0400

    11

--- EDIS_Project_Lavy/server.js
+++ EDIS_Project_Lavy/server.js
@@ -10,7 +10,7 @@ var cookieParser = require('cookie-parser');
 var mysql = require('mysql');
 
 //var port     = process.env.PORT || 8080;
-var port     = process.env.PORT || 7000;
+var port     = process.env.PORT || 9000;
 
 //redis variable
 var redis = require("redis");
